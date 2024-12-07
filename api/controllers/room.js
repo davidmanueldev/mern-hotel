31b85191ce117be48c976ac44d165278e20f35df
David Manuel <davidmanuel.dev@gmail.com>
2024-12-06 22:53:36 -0400

    Relaciones entre modelos de Node & MongoDB

--- api/controllers/room.js
+++ api/controllers/room.js
@@ -3,7 +3,7 @@ import Hotel from "../models/Hotel.js";
 import { createError } from "../utils/error.js";
 
 export const createRoom = async (req, res, next) => {
-  const hotelId = req.params.hotelId;
+  const hotelId = req.params.hotelid;
   const newRoom = new Room(req.body);
 
   try {
@@ -20,3 +20,51 @@ export const createRoom = async (req, res, next) => {
     next(err);
   }
 };
+
+export const updateRoom = async (req, res, next) => {
+  try {
+    const updatedRoom = await Room.findByIdAndUpdate(
+      req.params.id,
+      { $set: req.body },
+      { new: true }
+    );
+    res.status(200).json(updatedRoom);
+  } catch (err) {
+    next(err)
+  }
+}
+
+export const deleteRoom = async (req, res, next) => {
+  const hotelId = req.params.hotelid;
+  try {
+    await Room.findByIdAndDelete(req.params.id);
+    try {
+      await Hotel.findByIdAndUpdate(hotelId, {
+        $pull: { habitaciones: req.params.id },
+      });
+    } catch (err) {
+      next(err);
+    }
+    res.status(200).json("La habitación ha sido eliminada...");
+  } catch (err) {
+    next(err)
+  }
+}
+
+export const getRoom = async (req, res, next) => {
+  try {
+    const room = await Room.findById(req.params.id);
+    res.status(200).json(room);
+  } catch (err) {
+    next(err)
+  }
+}
+
+export const getRooms = async (req, res, next) => {
+  try {
+    const rooms = await Room.find();
+    res.status(200).json(rooms);
+  } catch (err) {
+    next(err)
+  }
+}
